39d8f5c1fe59fbf4e6e2f11e7588430c7ed571e7
Rafael da Silva <rafaeltimoreirass@gmail.com>
2026-01-27 23:19:54 -0300

    refactor: add <Link> href rota

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main className="min-h-screen flex items-center justify-center">
-      <h1 className="text-2xl font-semibold">
-        Participa DF – Ouvidoria Digital Acessível
-      </h1>
+      <Link href="/manifestacao" className="text-2xl font-semibold bg-blue-600 text-white px-8 py-4 rounded-lg hover:bg-blue-700 transition-colors shadow-lg">
+        <h1 className="text-2xl font-semibold">
+          Participa DF – Ouvidoria Digital Acessível
+        </h1>
+      </Link>
     </main>
   );
 }
